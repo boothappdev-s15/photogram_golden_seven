a6cf0cf27c23f81a31ecde583aee241c776f5402
odysseus5757 <stephen.cowen@gmail.com>
2015-05-07 15:19:38 -0500

    homework_create_and_read_complete_check_complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,8 +3,22 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def new_form
+  end
+
+  def create_row
+    p = Photo.new
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
   def show
      @specificphoto = Photo.find_by({ :id => params[:id] })
   end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  # route to CREATE new photos in the table
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+
 
 end
