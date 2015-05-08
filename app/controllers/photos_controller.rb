980383b5636c1b9ae9c3cb25004c1920dd0891fb
MarzB <mbrzakal@chicagobooth.edu>
2015-05-08 18:22:44 -0500

    Finished "Show" Photo Details Pages
    Completed photo details pages so that clicking on "show" takes you to
    the photo corresponding to the number after the second slash

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,14 @@
 class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
+
+    render('index.html.erb')
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id] })
+
+    render('show.html.erb')
+  end
 
 end
