42f7288f56df37c9851fec3978faa18f681c0abe
np1989 <np1989@gmail.com>
2015-05-09 11:09:05 -0500

    Update 1
    Added the following functions:
    
    Deleting Pictures
    Adding Pictures
    Working on Editing Pictures
    Added Link function to the pictures on the index page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,71 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+
+    @pic_id = params[:id]
+
+    p = Photo.find_by ({:id => @pic_id})
+
+    @caption = p.caption
+    @source = p.source
+
+  end
+
+  def new_form
+
+  end
+
+  def add_pic
+
+    p = Photo.new
+    @caption = params[:caption]
+    @source  = params[:source]
+    p.caption = @caption
+    p.source = @source
+    p.save
+
+    redirect_to('http://localhost:3000/photos/')
+
+  end
+
+  def delete
+
+    @pic_id = params[:id]
+
+    p = Photo.find_by ({:id => @pic_id})
+
+    p.destroy
+    p.save
+    redirect_to('http://localhost:3000/photos/')
+  end
+
+def edit
+
+  @pic_id = params[:id]
+
+    p = Photo.find_by ({:id => @pic_id})
+
+    @caption = p.caption
+    @source = p.source
+end
+
+def save_edit
+
+  @pic_id = params[:id]
+  @caption = params[:caption]
+  @source = params[:source ]
+
+    p = Photo.find_by ({:id => @pic_id})
+
+    p.caption = @caption
+    p.source = @source
+
+    p.save
+
+    redirect_to('http://localhost:3000/photos/<%= @pic_id %>')
+
+
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
+  get("/photos/new",       { :controller => "photos", :action => "new_form" })
+  get("/photos/create_new",{ :controller => "photos", :action => "add_pic" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos/:id/edit",  { :controller => "photos", :action => "edit" })
+  get("/photos/save_edit/:pic_id",  { :controller => "photos", :action => "save_edit" })
+  get("/delete_photo/:id", { :controller => "photos", :action => "delete" })
+
+
+
 
 end
