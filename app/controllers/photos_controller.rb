dd48618d6e4c03fb53c51774c9953482681ebc7b
wulfas <afitzger@chicagobooth.edu>
2015-05-09 15:26:23 -0500

    update_completed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -15,6 +15,29 @@ class PhotosController < ApplicationController
     redirect_to("http://localhost:3000/photos")
   end
 
+  def destroy
+    i = Photo.find(params[:id])
+    i.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({ :id => params[:id]})
+
+  end
+
+  def  update_form
+    i = Photo.find(params[:id])
+    i.source = params[:the_source]
+    i.caption = params[:the_caption]
+    i.save
+
+ @photo = Photo.find_by({ :id => params[:id]})
+
+  end
+
+
   def show
     @photo = Photo.find_by({ :id => params[:id]})
   end
