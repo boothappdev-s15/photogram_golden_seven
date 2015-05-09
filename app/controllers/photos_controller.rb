6b3cb08a40694a92a961832e92cd5c91d4d22bfb
wulfas <afitzger@chicagobooth.edu>
2015-05-09 14:21:23 -0500

    create_completed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,9 +3,20 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def new_form
+  end
+
+  def create_row
+    i = Photo.new
+    i.source = params[:the_source]
+    i.caption = params[:the_caption]
+    i.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
   def show
     @photo = Photo.find_by({ :id => params[:id]})
-
   end
 
 end
