1738b62e9217155833e1b0f2f93f5254e9c3c104
rohitmenon83 <rohitmenon83@yahoo.com>
2015-05-09 16:16:59 -0500

    Completed Create
    Added the actions that are required to create a new photo and save it.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,13 +5,22 @@ class PhotosController < ApplicationController
   end
 
  def show
-
-
     @source= Photo.find_by({:id => params[:id]}).source
     @caption= Photo.find_by({:id => params[:id]}).caption
-
-
  end
 
+  def new_form
+
+  end
+
+  def create_row
+    z=Photo.new
+    z.caption=params[:the_caption]
+    z.source=params[:the_source]
+    z.save
+
+  # redirect_to("http://localhost:3000/photos")
+
+  end
 
 end
