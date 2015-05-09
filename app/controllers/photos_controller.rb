5445f68e254b8c025d9dd9d5b7cf4a23504df374
evazquezt <evazquez@chicagobooth.edu>
2015-05-09 15:30:23 -0500

    completed_delete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,31 @@
 class PhotosController < ApplicationController
+
   def index
     @list_of_photos = Photo.all
   end
 
+  def show
+    @list_of_photos = Photo.all
+    @photo = Photo.find_by({:id => params[:id]})
+    render('show.html.erb')
+  end
+
+  def edit
+    @list_of_photos = Photo.all
+    @photo = Photo.find_by({:id => params[:id]})
+    render('edit.html.erb')
+  end
+
+  def delete
+    d = Photo.find(params[:id])
+    d.destroy
+    redirect_to('http://localhost:3000/photos')
+  end
+
+  def new
+    @list_of_photos = Photo.all
+    @photo = Photo.find_by({:id => params[:id]})
+    render('new.html.erb')
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   get("/", { :controller => "photos", :action => "index" })
 
   # Routes to READ photos
+
   get("/photos",           { :controller => "photos", :action => "index" })
+
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
+  get("/photos/:id/edit",       { :controller => "photos", :action => "edit" })
+
+  get("/delete_photo/:id",       { :controller => "photos", :action => "delete" })
+
+  get("/photos/new",       { :controller => "photos", :action => "new" })
+
 end
+
