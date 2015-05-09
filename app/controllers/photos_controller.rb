934e1367b9e479622bb197b34322e1d6f1f78162
MarzB <mbrzakal@chicagobooth.edu>
2015-05-08 19:18:19 -0500

    Finished through "Delete"
    Finished through "Delete", though when I add a new row, my captions and
    picture are not appearing. Will fix.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,10 +5,31 @@ class PhotosController < ApplicationController
     render('index.html.erb')
   end
 
+  def new_form
+
+    render('new_form.html.erb')
+  end
+
+  def create_row
+    p = Photo.new
+    p.source = params[:photo_caption]
+    p.caption = params[:photo_image_url]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
   def show
     @photo = Photo.find_by({ :id => params[:id] })
 
     render('show.html.erb')
+  end
+
+  def destroy
+    z = Photo.find(params[:id])
+    z.destroy
+
+    redirect_to("http://localhost:3000/photos")
   end
 
 end
