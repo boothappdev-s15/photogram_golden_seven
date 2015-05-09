974c6d9634f3d955956431b30145390cc73f3787
rohitmenon83 <rohitmenon83@yahoo.com>
2015-05-09 12:56:59 -0500

    Completed Step I (Show)
    Completed section of the HW that deals with showing the picture loaded
    using dynamic routing.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,17 @@
 class PhotosController < ApplicationController
+
   def index
     @list_of_photos = Photo.all
   end
+
+ def show
+
+
+    @source= Photo.find_by({:id => params[:id]}).source
+    @caption= Photo.find_by({:id => params[:id]}).caption
+
+
+ end
 
 
 end
