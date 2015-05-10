90acf100154bf0086d3eafae0c25ddc82db20ec7
mpoelst <mpoelstr@chicagobooth.edu>
2015-05-10 14:19:12 -0500

    finished show images and add new photo
    finished updating picture for "show images"
    
    finished links for "add a new photo"

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,6 +9,18 @@ class PhotosController < ApplicationController
   end
 
   def new_form
+    # render('new_form.html.erb')
+  end
+
+  def create_row
+
+  #  raise params.inspect
+  p = Photo.new
+  p.source = params[:the_source]
+  p.caption = params[:the_caption]
+  p.save
+
+  redirect_to("http://localhost:3000/photos")
   end
 
 end
