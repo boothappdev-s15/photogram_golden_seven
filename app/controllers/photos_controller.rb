a3ae568404fcdbcb5bd73cfc272ceddf306dbc36
AshleyHawn <ahawn@chicagobooth.edu>
2015-05-11 16:13:36 -0500

    Part 2 in process

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,16 @@ class PhotosController < ApplicationController
     @photo=Photo.find(params[:id])
   end
 
+  def new_form
+
+  end
+
+  def create_row
+    p=Photo.new
+    p.caption = params[:caption]
+    p.url = params[:image_url]
+    p.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get("/", { :controller => "photos", :action => "index" })
 
   # Routes to READ photos
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
