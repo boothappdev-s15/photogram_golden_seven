dca679dab8cbbac55674bfb9221712a7bb2bd1e1
Larsam123 <larsen.samuel@gmail.com>
2015-05-10 23:57:36 -0500

    show_picture_and_add_row

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,29 @@ class PhotosController < ApplicationController
   end
 
 
+  def show
+    id = params[:id].to_i
+
+    @p = Photo.find(id)
+
+    @source = @p.source
+    @caption = @p.caption
+  end
+
+
+  def new_form
+  end
+
+  def create_row
+    p = Photo.new
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  #Add new photos
+  get("/photos/new",       { :controller => "photos", :action => "new_form"})
+  get("/photos/create_row", { :controller => "photos", :action => "create_row"})
+
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
 
 end
