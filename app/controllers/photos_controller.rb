8d66380c00dd15e7bad7a08f3c7e86fcbcda4941
edombrowski <Katie@Katies-MacBook-Pro-3.local>
2015-05-11 21:19:27 -0500

    final commit
    final commit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,41 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def new_form
+  end
+
+  def edit_form
+    i = Photo.find_by({ :id => params[:id]})
+    @photo = i
+  end
+
+def update_row
+  i = Photo.find_by({ :id => params[:id]})
+  i.source = params["the_source"]
+  i.caption = params["the_caption"]
+  i.save
+  redirect_to("http://localhost:3000/photos/#{i.id}")
+end
+
+
+  def destroy
+    i = Photo.find_by({ :id => params[:id]})
+    i.destroy
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def create_row
+    i = Photo.new
+    i.source = params["the_source"]
+    i.caption = params["the_caption"]
+    i.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def show
+    i = Photo.find_by({ :id => params[:id]})
+    @image = i.source
+    @caption = i.caption
+  end
 
 end
