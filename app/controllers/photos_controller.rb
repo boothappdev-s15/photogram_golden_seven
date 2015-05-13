d97c63c61bd548f7f87046fe2e9c2baa7bbd8569
MarzB <mbrzakal@chicagobooth.edu>
2015-05-13 11:13:09 -0500

    Finished Assignment
    Homework completed.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,11 +10,27 @@ class PhotosController < ApplicationController
     render('new_form.html.erb')
   end
 
+  def edit_form
+    @photo_id = params[:id]
+    p = Photo.find_by({ :id => @photo_id })
+    @caption = p.caption
+    @source = p.source
+  end
+
+  def update_row
+    @photo = Photo.find(params[:id])
+    @photo.source = params[:the_source]
+    @photo.caption = params[:the_caption]
+    @photo.save
+
+    render('show.html.erb')
+  end
+
   def create_row
-    p = Photo.new
-    p.source = params[:photo_caption]
-    p.caption = params[:photo_image_url]
-    p.save
+    photo = Photo.new
+    photo.source = params[:the_source]
+    photo.caption = params[:the_caption]
+    photo.save
 
     redirect_to("http://localhost:3000/photos")
   end
